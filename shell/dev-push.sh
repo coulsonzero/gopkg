a89121597d80de2c5f01813d11b32c269a9e9726
coulsonzero <coulson2021@163.com>
2022-10-10 11:20:33 +0800

    [update]: modfiy shell

--- shell/dev-push.sh
+++ shell/dev-push.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
+main() {
+  git checkout dev
+  git add .
+  git commit -m "[update]: ${message}"
+  #git pull origin dev --rebase
+  git push origin dev
+}
+
 # shellcheck disable=SC2162
 read -t 30 -p "[dev] Enter commit >>> " message
 if [[ $message != "" ]]; then
   echo "[dev] Success added a commit: ${message}"
+  main
 else
   echo "[dev] No commit input !"
 fi
 
-git checkout dev
-git add .
-git commit -m "[update]: ${message}"
-#git pull origin dev --rebase
-git push origin dev
 echo '------------------------'
