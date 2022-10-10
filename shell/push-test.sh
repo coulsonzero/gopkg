087161fd80c711e7cc5ce14ba99c54feb2e85371
coulsonzero <coulson2021@163.com>
2022-10-10 15:04:55 +0800

    [update]: modify push.sh

--- shell/push-test.sh
+++ shell/push-test.sh
@@ -29,3 +29,25 @@ else
     命令示例: sh push.sh dev
     "
 fi
+
+
+#   if [ "$1" = "dev" ]; then
+#      bash ./shell/dev-push.sh
+#   elif [ "$1" = "master" ]; then
+#      bash ./shell/master-push.sh
+#   elif [ "$1" = 'both' ]; then
+#      bash ./shell/dev-push.sh && bash ./shell/master-push.sh
+#   elif [ "$1" = 'tag' ]; then
+#       # 判断第二个输入参数为'-d' ?
+#       if [ "$2" = '-d' ]; then
+#           bash ./shell/tag-delete.sh
+#       # 判断第二个输入参数 ?
+#       elif [ -z "$2" ]; then
+#           bash ./shell/tag-release.sh
+#       # 只有一个输入参数
+#       else
+#           echo 'undefined the second argument '
+#       fi
+#   else
+#       echo "$1 No input"
+#  fi
