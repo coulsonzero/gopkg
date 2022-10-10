40c4dfd9b4c5ed5d3056434af6e95b50021000a0
coulsonzero <coulson2021@163.com>
2022-10-10 09:47:26 +0800

    update: modify `dev-push.sh` script

--- dev-push.sh
+++ dev-push.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-message=$*
+read -p "[dev] Enter commit >>> " message
+if [[ $message != "" ]]; then
+  echo "add a commit: ${message}"
+else
+  echo  'No commit input !'
+fi
 
 git checkout dev
 git add .
