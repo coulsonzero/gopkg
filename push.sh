d5e5b43852504d4f038b29222875ec1f6d8405cf
coulsonzero <coulson2021@163.com>
2022-10-10 10:56:03 +0800

    [update]: modify shell

---
+++ push.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+if [ ${#} -ge 1 ]
+then
+    case ${1} in
+        "dev")
+            bash dev-push.sh
+        ;;
+        "master")
+            bash master-push.sh
+        ;;
+        "both")
+            bash dev-push.sh > bash master-push.sh
+        ;;
+        "tag")
+            bash tag-release.sh
+        ;;
+        "tag -d")
+            bash tag-delete.sh
+        ;;
+        *)
+            echo "${1}No input"
+        ;;
+    esac
+else
+    echo "
+    command如下命令:
+    dev    : 推送dev
+    master : 推送master
+    tag    : 推送tag
+
+    命令示例: sh push.sh dev
+    "
+fi
