b4eede8727c2eeee3f8e1daf7dbbc52a96628378
coulsonzero <coulson2021@163.com>
2022-10-10 12:20:58 +0800

    [update]: modify the shell: push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# 判断输入参数数量 > 1 ？
 if [ ${#} -ge 1 ]; then
    if [[ ${1} == 'dev' ]]; then
       bash ./shell/dev-push.sh
@@ -8,23 +9,27 @@ if [ ${#} -ge 1 ]; then
    elif [[ ${1} == 'both' ]]; then
       bash ./shell/dev-push.sh && bash ./shell/master-push.sh
    elif [[ ${1} == 'tag' ]]; then
+       # 判断第二个输入参数为'-d' ?
        if [[ ${2} == '-d' ]]; then
            bash ./shell/tag-delete.sh
-       elif [[ -n ${2} ]]; then
-           echo 'undefined the second argument '
-       else
+       # 判断第二个输入参数 ?
+       elif [[ -z ${2} ]]; then
            bash ./shell/tag-release.sh
+       # 只有一个输入参数
+       else
+           echo 'undefined the second argument '
        fi
    else
        echo "${1}No input"
   fi
 else
     echo "
-    command如下命令:
+    [Bash基础命令]:
     dev    : 推送dev
     master : 推送master
+    both   : 同时推送dev与master
     tag    : 推送tag
-    tag-d  : 删除远程tag
+    tag -d : 删除tag
 
     命令示例: sh push.sh dev
     "
