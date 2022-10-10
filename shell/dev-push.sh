56417f9f621563bae0ae2e9b45072e4be6584ad6
coulsonzero <coulson2021@163.com>
2022-10-11 00:58:40 +0800

    [update]: modify README.md(v0.8.1-beta)

--- shell/dev-push.sh
+++ shell/dev-push.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
 
-function main() {
-  git checkout dev
-  git add . && git commit -m "[update]: $message"
-  git pull origin dev --rebase
-  git push origin dev
-  exit 0
-}
+set -e
 
 # shellcheck disable=SC2162
 read -t 30 -p "[dev] Enter commit >>> " message
 if [ "$message" != "" ]; then
   echo "[dev] Success added a commit: $message"
-  main
 else
   echo "[dev] No commit input !"
   exit 1
 fi
 
-
+git checkout dev
+git add .
+git commit -m "[update]: $message"
+git pull origin dev --rebase
+git push origin dev
+exit 0
 
 echo '------------------------'
