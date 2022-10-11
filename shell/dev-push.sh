1892f2db6cf039b21cf9a841e97a7651f9d53222
coulsonzero <coulson2021@163.com>
2022-10-11 22:01:56 +0800

    [update]: modify dev-push.sh

--- shell/dev-push.sh
+++ shell/dev-push.sh
@@ -9,7 +9,7 @@ read -t 30 -p "[dev] Enter commit >>> " message
 if [ "$message" != "" ]; then
   git commit -m "[update]: $message"
   echo "[dev] Success added a commit: $message"
-  git pull
+  git pull origin dev
   git push origin dev
   exit 0
 else
