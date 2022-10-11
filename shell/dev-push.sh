cf98aa356c1aa681a2e9246b0bb10ecc79ca758d
coulsonzero <coulson2021@163.com>
2022-10-11 21:56:35 +0800

    [update]: modify runtim files commits

--- shell/dev-push.sh
+++ shell/dev-push.sh
@@ -6,16 +6,15 @@ set -e
 read -t 30 -p "[dev] Enter commit >>> " message
 if [ "$message" != "" ]; then
   echo "[dev] Success added a commit: $message"
+  git checkout dev
+  git add .
+  git commit -m "[update]: $message"
+  git pull origin dev --rebase
+  git push origin dev
 else
   echo "[dev] No commit input !"
   exit 1
 fi
-
-git checkout dev
-git add .
-git commit -m "[update]: $message"
-git pull origin dev --rebase
-git push origin dev
 
 
 echo '------------------------'
