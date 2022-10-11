f7f787293307c21097b98e2bde4e24cf0df22b0d
coulsonzero <coulson2021@163.com>
2022-10-11 22:06:26 +0800

    [update]: modify dev-push.sh

--- shell/dev-push.sh
+++ shell/dev-push.sh
@@ -6,16 +6,15 @@ git checkout dev
 git add .
 git status
 # shellcheck disable=SC2162
-read -t 30 -p "[dev] Enter commit >>> " message
+read -t 40 -p "[dev] Enter commit >>> " message
 if [ "$message" != "" ]; then
   git commit -m "[update]: $message"
-  echo "[dev] Success added a commit: $message"
   git pull origin dev
   git push origin dev
   exit 0
 else
   git reset
-  echo "[dev] No commit input !"
+  echo "[dev] You haven't entered any comments !"
   exit 1
 fi
 git checkout dev
