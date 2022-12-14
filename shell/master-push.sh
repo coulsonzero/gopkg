2050e85b47bcf99e4cc07fca3be9cebe17942ed9
coulsonzero <coulson2021@163.com>
2022-12-14 19:54:37 +0800

    [update]: modify pro: math, slice, sort, time and so on package

--- shell/master-push.sh
+++ shell/master-push.sh
@@ -2,7 +2,8 @@
 
 git checkout master
 git merge dev
-git pull origin master --rebase
+git pull origin master
+#git pull origin master --rebase
 #git pull origin master
 git push origin master
 git checkout dev
