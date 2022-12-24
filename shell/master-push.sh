d6199ba361ff520fa36a50539023aee6bb970bf3
coulsonzero <coulson2021@163.com>
2022-12-24 16:09:48 +0800

    [update]: modify master-push.sh

--- shell/master-push.sh
+++ shell/master-push.sh
@@ -4,7 +4,6 @@ git checkout master
 git merge dev
 git pull origin master
 #git pull origin master --rebase
-#git pull origin master
 git push origin master
 git checkout dev
 echo '------------------------'
